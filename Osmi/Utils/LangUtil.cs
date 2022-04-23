058057093e5cc7a23e9600822d46748ba83d43df
Frank Yang <clazex.y@gmail.com>
2022-04-23 12:50:17 +0800

    feat: lang util

---
+++ Osmi/Utils/LangUtil.cs
@@ -0,0 +1,33 @@
+using Language;
+
+namespace Osmi.Utils;
+
+[PublicAPI]
+public static class LangUtil {
+	public static List<string> Langs { get; } = Lang
+		.GetLanguages()
+		.Map(str => str.ToLower().Replace('_', '-'))
+		.ToList();
+
+	public static string ToIdentifier(this LanguageCode code) =>
+		code.ToString().ToLower().Replace('_', '-');
+
+	public static string CurrentLang =>
+		Lang.CurrentLanguage().ToIdentifier();
+
+	public static Dictionary<string, Lazy<Dictionary<string, string>>> LoadLangs(
+		Assembly asm,
+		IEnumerable<(string lang, string path)> tuples
+	) => tuples.ToDictionary(
+		tuple => tuple.lang,
+		tuple => new Lazy<Dictionary<string, string>>(() => {
+			string content = asm
+				.GetManifestResourceStream(tuple.path).ReadToString();
+			Dictionary<string, string> table =
+				JsonConvert.DeserializeObject<Dictionary<string, string>>(content)!;
+
+			Logger.LogDebug($"Loaded localization for lang {tuple.lang} in {asm.GetName().Name}");
+			return table;
+		})
+	);
+}
