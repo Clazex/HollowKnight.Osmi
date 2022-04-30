2791cdd5444871f2aee81c6b90942d197c6ff7ef
Frank Yang <clazex.y@gmail.com>
2022-04-30 19:44:28 +0800

    feat: add static method shortcut in string util

--- Osmi/Utils/StringUtil.cs
+++ Osmi/Utils/StringUtil.cs
@@ -68,4 +68,11 @@ public static class StringUtil {
 
 		return builder.ToString();
 	}
+
+
+	public static bool IsNullOrEmpty(this string self) =>
+		string.IsNullOrEmpty(self);
+
+	public static bool IsNullOrWhiteSpace(this string self) =>
+		string.IsNullOrWhiteSpace(self);
 }
