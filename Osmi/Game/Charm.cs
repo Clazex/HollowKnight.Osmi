c154ebdc0b5109bea5f2efeff15e4b3c74ece89c
Frank Yang <clazex.y@gmail.com>
2022-04-30 19:46:09 +0800

    feat: charm enum

---
+++ Osmi/Game/Charm.cs
@@ -0,0 +1,48 @@
+namespace Osmi.Game;
+
+[PublicAPI]
+public enum Charm {
+	WaywardCompass = 2,
+	GatheringSwarm = 1,
+	StalwartShell = 4,
+	SoulCatcher = 20,
+	ShamanStone = 19,
+	SoulEater = 21,
+	Dashmaster = 31,
+	Sprintmaster = 37,
+	Grubsong = 3,
+	GrubberflysElegy = 35,
+
+	UnbreakableHeart = 23,
+	UnbreakableGreed = 24,
+	UnbreakableStrength = 25,
+	SpellTwister = 33,
+	SteadyBody = 14,
+	HeavyBlow = 15,
+	QuickSlash = 32,
+	Longnail = 18,
+	MarkOfPride = 13,
+	FuryOfTheFallen = 6,
+
+	ThornsOfAgony = 12,
+	BaldurShell = 5,
+	Flukenest = 11,
+	DefendersCrest = 10,
+	GlowingWomb = 22,
+	QuickFocus = 7,
+	DeepFocus = 34,
+	LifebloodHeart = 8,
+	LifebloodCore = 9,
+	JonisBlessing = 27,
+
+	Hiveblood = 29,
+	SporeShroom = 17,
+	SharpShadow = 16,
+	ShapeOfUnn = 28,
+	NailmastersGlory = 26,
+	Weaversong = 39,
+	DreamWielder = 30,
+	Dreamshield = 38,
+	Grimmchild = 40,
+	VoidHeart = 36
+}
