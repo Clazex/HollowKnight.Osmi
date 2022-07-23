185ebd567facc5297c438a1f499e07ea861c5d49
Frank Yang <clazex.y@gmail.com>
2022-07-23 23:50:16 +0800

    perf: cache nullability

--- Osmi/Game/ChaseObject.cs
+++ Osmi/Game/ChaseObject.cs
@@ -4,13 +4,22 @@ namespace Osmi.Game;
 [RequireComponent(typeof(Rigidbody2D))]
 public class ChaseObject : MoveToPosition2D {
 	public override Vector2? TargetPos {
-		get => Target?.position.AsVector2();
+		get => isNull ? null : Target!.position.AsVector2();
 		set {
-			if (value.HasValue) {
-				Target?.SetPosition2D(value.Value.x, value.Value.y);
+			if (value.HasValue && !isNull) {
+				Target!.SetPosition2D(value.Value.x, value.Value.y);
 			}
 		}
 	}
 
-	public Transform? Target { get; set; } = null;
+	private bool isNull = true;
+	private Transform? tf = null!;
+
+	public Transform? Target {
+		get => tf;
+		set {
+			isNull = value == null;
+			tf = value;
+		}
+	}
 }
